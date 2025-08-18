b96e0b46d91facfd401533ad2e6044cd30d3a50b
BeLieVer031 <sandyrajak031@gmail.com>
2025-08-18 17:56:25 +0530

    feat: add seprate line

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
       <div className="bg-background text-primary">
         <Navbar />
         <Hero />
+        <div className="h-px w-full bg-neutral-700"></div>
       </div>
     </main>
   )
