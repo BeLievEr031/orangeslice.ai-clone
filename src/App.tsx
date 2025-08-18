b0948846206ad35072842a9faa8a4359aad47b91
BeLieVer031 <sandyrajak031@gmail.com>
2025-08-18 18:06:33 +0530

    feat: add footer.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar"
+import Footer from "./sections/Footer"
 import Hero from "./sections/Hero"
 import InsightHighlight from "./sections/InsightHighlight"
 import SalesAgentSection from "./sections/SalesAgentSection"
@@ -12,6 +13,7 @@ function App() {
         <div className="h-px w-full bg-neutral-700"></div>
         <SalesAgentSection />
         <InsightHighlight />
+        <Footer />
       </div>
     </main>
   )
